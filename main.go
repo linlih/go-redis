95d6336502bc6b3feadcfc36256ce7439e6a9e5a
linlh <michael_llh@163.com>
2022-08-28 16:56:00 +0800

    :white_check_mark: tcp echo

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"go-redis/config"
+	"go-redis/lib/logger"
+	"go-redis/tcp"
+	"os"
+)
+
+const configFile string = "redis.conf"
+
+var defaultProperites = &config.ServerProperties{
+	Bind: "0.0.0.0",
+	Port: 6379,
+}
+
+func fileExist(filename string) bool {
+	info, err := os.Stat(filename)
+	return err == nil && !info.IsDir()
+}
+
+func main() {
+	//logger.Setup(&logger.Settings{
+	//	Path:       "logs",
+	//	Name:       "go-redis",
+	//	Ext:        "log",
+	//	TimeFormat: "2022-08-28",
+	//})
+
+	if fileExist(configFile) {
+		config.SetupConfig(configFile)
+	} else {
+		config.Properties = defaultProperites
+	}
+	err := tcp.ListenAndServeWithSignal(&tcp.Config{
+		Address: fmt.Sprintf("%s:%d", config.Properties.Bind, config.Properties.Port),
+	}, tcp.NewEchoHandler())
+	if err != nil {
+		logger.Fatal(err)
+	}
+}
---
+++ tcp/server.go
@@ -0,0 +1,65 @@
+package tcp
+
+import (
+	"context"
+	"go-redis/interface/tcp"
+	"go-redis/lib/logger"
+	"net"
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+type Config struct {
+	Address string
+}
+
+func ListenAndServeWithSignal(cfg *Config, handler tcp.Handler) error {
+	listener, err := net.Listen("tcp", cfg.Address)
+	closeChan := make(chan struct{})
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+	go func() {
+		sig := <-sigChan
+		switch sig {
+		case syscall.SIGHUP, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT:
+			closeChan <- struct{}{}
+		}
+	}()
+	if err != nil {
+		return err
+	}
+	logger.Info("start listen")
+	ListenAndServe(listener, handler, closeChan)
+	return nil
+}
+
+func ListenAndServe(listener net.Listener, handler tcp.Handler, closeChan <-chan struct{}) {
+	go func() {
+		<-closeChan
+		logger.Info("shutting down")
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
+	defer func() {
+		_ = listener.Close()
+		_ = handler.Close()
+	}()
+
+	ctx := context.Background()
+	var waitDone sync.WaitGroup
+	for true {
+		conn, err := listener.Accept()
+		if err != nil {
+			break
+		}
+		logger.Info("accepted new connection")
+		waitDone.Add(1)
+		go func() {
+			defer waitDone.Done() // 这里需要注意！不要在handle下面写，因为如果 handle 出现了 panic，那就无法执行到 waitDone.Done 了，所以这里需要使用defer
+			handler.Handle(ctx, conn)
+		}()
+	}
+	waitDone.Wait()
+}
